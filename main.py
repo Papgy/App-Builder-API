3363be4bcad3f45e516cff988f824d7b2901c13f
Papgy100 <123894339+Papgy@users.noreply.github.com>
2025-04-12 12:47:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=['https://papgy.github.io'],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=['Access-Control-Allow-Origin', "*"],
 )
 
 # ✅ Load text gen model
